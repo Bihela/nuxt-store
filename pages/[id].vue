9ed296bb40e677eb935c852ccd9a8e490a1130f1
Bihela <sandeshwanasekara@gmail.com>
2024-03-25 13:19:06 +0530

    Initial Home page

---
+++ pages/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>({ product.title})</p>
+        <p>({ product.price})</p>
+        <p>({ product.id})</p>
+    </div>
+</template>
+
+<script setup>
+    const {id} = useRoute().params
+    const url = 'https://fakestoreapi.com/products' + id
+    const { data: product} = await useFetch(uri, {key: id})
+    definePageMeta({layout: 'products'})
+</script>
+
+<style  scoped>
+
+</style>
