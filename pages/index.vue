b450a822144e1d2b59a4cd6ac9a611a3633ecdb7
Bihela <sandeshwanasekara@gmail.com>
2024-03-27 09:57:33 +0530

    Fix Final Version

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,48 @@
-c
+<template>
+    <div class>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis omnis consequatur quod necessitatibus autem repellat aperiam quia atque fugit praesentium earum rerum minus, porro asperiores explicabo inventore animi ducimus. Itaque voluptatum facilis nobis, praesentium, quasi pariatur veniam veritatis quo eligendi, ratione libero impedit. Distinctio, delectus sapiente adipisci assumenda obcaecati aliquid exercitationem sit ad autem! Veritatis nihil accusamus recusandae esse?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis omnis consequatur quod necessitatibus autem repellat aperiam quia atque fugit praesentium earum rerum minus, porro asperiores explicabo inventore animi ducimus. Itaque voluptatum facilis nobis, praesentium, quasi pariatur veniam veritatis quo eligendi, ratione libero impedit. Distinctio, delectus sapiente adipisci assumenda obcaecati aliquid exercitationem sit ad autem! Veritatis nihil accusamus recusandae esse?</p>
+    </div>
+</template>
+
+
+  
+  <style scoped>
+    h2 {
+      margin-bottom: 20px;
+      font-size: 36px;
+    }
+    p {
+      margin: 20px 0;
+    }
+    
+  div {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+    color: #333;
+  }
+
+  p {
+    margin-bottom: 20px;
+    line-height: 1.6;
+  }
+
+
+  p:hover {
+    background-color: #f0f0f0;
+  }
+
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f9f9f9;
+  }
+
+
+  </style>
